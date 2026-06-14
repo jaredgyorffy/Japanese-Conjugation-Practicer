6dd51154270291cde85a72992c31619b77d73578
Jared Gyorffy <jaredgyorffy42@gmail.com>
2026-06-13 22:14:04 -0300

    Added a couple of adjective conjugations

--- Assets/2. Scripts/Interfaces/Adjective.cs
+++ Assets/2. Scripts/Interfaces/Adjective.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
 using NaughtyAttributes;
@@ -17,7 +17,93 @@ public class Adjective : IWord
     [SerializeField] private AdjectiveType adjectiveType;
     public List<string> Meaning => meaning;
     [SerializeField] private List<string> meaning;
+
+    public string PolitePast => ConjugatePolitePast(kana);
+    [Foldout("Irregular Conjugation")][ShowIf("AdjectiveType", AdjectiveType.IRR)][SerializeField][AllowNesting] private string politePast;
+
+    public string StandardPast => ConjugatePolitePast(kana);
+    [Foldout("Irregular Conjugation")][ShowIf("AdjectiveType", AdjectiveType.IRR)][SerializeField][AllowNesting] private string standardPast;
+
+    public string StandardNonpastNegative => ConjugateStandardNonpastNegative(kana);
+    [Foldout("Irregular Conjugation")][ShowIf("AdjectiveType", AdjectiveType.IRR)][SerializeField][AllowNesting] private string standardNonpastNegative;
+
+    public string PoliteNonpastNegative => ConjugatePoliteNonpastNegative(kana);
+    [Foldout("Irregular Conjugation")][ShowIf("AdjectiveType", AdjectiveType.IRR)][SerializeField][AllowNesting] private string politeNonpastNegative;
+    public string StandardNonpast => kana;
+
+    private string ConjugatePolitePast(string dictionaryForm)
+    {
+        string conjugatedForm = "";
+        if (AdjectiveType == AdjectiveType.I)
+        {
+            conjugatedForm = dictionaryForm.Substring(0, dictionaryForm.Length - 1) + "かったです";
+        }
+        else if (AdjectiveType == AdjectiveType.NA)
+        {
+            conjugatedForm = dictionaryForm.Substring(0, dictionaryForm.Length - 1) + "でした";
+        }
+        else if (AdjectiveType == AdjectiveType.IRR)
+        {
+            return politePast;
+        }
+        return conjugatedForm;
+    }
+
+    private string ConjugateStandardPast(string dictionaryForm)
+    {
+        string conjugatedForm = "";
+        if (AdjectiveType == AdjectiveType.I)
+        {
+            conjugatedForm = dictionaryForm.Substring(0, dictionaryForm.Length - 1) + "かった";
+        }
+        else if (AdjectiveType == AdjectiveType.NA)
+        {
+            conjugatedForm = dictionaryForm.Substring(0, dictionaryForm.Length - 1) + "だった";
+        }
+        else if (AdjectiveType == AdjectiveType.IRR)
+        {
+            return standardPast;
+        }
+        return conjugatedForm;
+    }
+
+    private string ConjugateStandardNonpastNegative(string dictionaryForm)
+    {
+        string conjugatedForm = "";
+        if (AdjectiveType == AdjectiveType.I)
+        {
+            conjugatedForm = dictionaryForm.Substring(0, dictionaryForm.Length - 1) + " じゃない";
+        }
+        else if (AdjectiveType == AdjectiveType.NA)
+        {
+            conjugatedForm = dictionaryForm + "じゃない";
+        }
+        else if (AdjectiveType == AdjectiveType.IRR)
+        {
+            return standardNonpastNegative;
+        }
+        return conjugatedForm;
+    }
+
+    private string ConjugatePoliteNonpastNegative(string dictionaryForm)
+    {
+        string conjugatedForm = "";
+        if (AdjectiveType == AdjectiveType.I)
+        {
+            conjugatedForm = dictionaryForm.Substring(0, dictionaryForm.Length - 1) + " くない";
+        }
+        else if (AdjectiveType == AdjectiveType.NA)
+        {
+            conjugatedForm = dictionaryForm + "じゃありません";
+        }
+        else if (AdjectiveType == AdjectiveType.IRR)
+        {
+            return standardNonpastNegative;
+        }
+        return conjugatedForm;
+    }
 }
+
 
 public enum AdjectiveType
 {
